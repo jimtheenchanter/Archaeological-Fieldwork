8fd4d876590b9887ebb548c0ef2d95190145c84e
Jim O'Brien <jimobrien@mailbox.org>
2019-10-10 22:02:01 +0100

    Basic Hillforts here! greeting

---
+++ app/src/main/java/ie/jim/hillforthere/HillfortActivity.kt
@@ -0,0 +1,12 @@
+package ie.jim.hillforthere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HillfortActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+    }
+}
